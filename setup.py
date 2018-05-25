5edfe59d7bbe199d12312a1a037b0821cf05cc97
Taylor Peer <t.peer@cortical.io>
2018-05-25 22:24:21 +0200

    Updated setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from setuptools import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-setup(
-    name='midi_to_dataframe',
-    version='0.1',
-    install_requires=[
-        'midi', 'pandas',
-    ],
-)
+# Define version information
+VERSION = '0.1'
+FULLVERSION = VERSION
+
+requirements = [
+]
+
+setup(name='midi_to_dataframe',
+      version=VERSION,
+      # TODO: description="",
+      author='Taylor Peer',
+      # TODO: author_email='',
+      url='https://github.com/TaylorPeer/midi-to-dataframe',
+      # TODO: license='',
+      packages=find_packages(),
+      install_requires=requirements,
+      package_data={},
+      # TODO: classifiers=[]
+      )
